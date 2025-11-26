b97020afba8008eaf1478d40b576851748bc741e
orione99 <mazzotta.gianni@gmail.com>
2025-11-26 20:13:03 +0100

    Esercizi Corso

---
+++ esercizi2.py
@@ -0,0 +1,24 @@
+#creo due varibili di valore 7 e 3, e le sommo
+x = 7
+y = 3 
+somma = x + y
+print(somma)
+
+# creo variabile prezzo (19.99) e una quantità (3)
+#stampo il totale
+prezzo = 19.99
+quantità = 3
+totale = prezzo * quantità
+print(totale)
+
+#creo una variabile numero (10)
+#stampo due esempi di True e False
+numero = 10
+print(numero > 5)
+print(numero < 5)
+
+#somma tra int (5) e float (2.5)
+x = 5
+y = 2.5
+somma = x + y
+print(somma)
---
+++ esercizio1.py
@@ -0,0 +1,11 @@
+#creo una variabile e la stampo
+Nome = 'Giovanni'
+print(Nome)
+
+#input richiesta di età e stampo raddoppiata
+eta = int(input('quanti anni hai?'))
+print(eta * 2)
+
+#creo due numeri e stampo la loro somma
+n1,n2 = 15,22
+print('La loro somma è', n1 + n2)
