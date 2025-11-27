4b59e7c0971f8b765e9daf139c55b5812a04a793
orione99 <mazzotta.gianni@gmail.com>
2025-11-27 18:30:09 +0100

    Create esercizi_var_casting_tipi_di_dati.py

---
+++ esercizi_var_casting_tipi_di_dati.py
@@ -0,0 +1,69 @@
+#esercizio numero 1
+nome = 'Giovanni'
+età = 26
+città = 'Pistoia'
+print(nome, età, città)
+
+#esercizio numero 2
+x = 10
+print(x)
+x = 74
+print(x)
+
+#esercizio numero 3
+x = 35
+y = 25
+print('somma:',x + y)
+
+#esercizio numero 4
+x = 25
+y = 45
+x,y = y,x
+print(x,y)
+
+#esercizio numero 5
+base = 10
+altezza = 5
+area = base * altezza
+print('area del rettangolo:', area)
+
+#esercizio numero 6
+x = 6
+y = 3.5
+print(x + y)
+
+#esercizio numero 7
+x,y,z = 10,20,30
+media = int((x + y + z)/3)
+print(media)
+
+#esercizio numero 8
+s1 = 'sto studiando'
+s2 = ' python'
+print(s1 + s2)
+
+#esercizio numero 9
+print('python '*3)
+
+#esercizio numero 10
+
+x,y,z = 10,20,30
+print(x < y)
+print(x > z)
+print(y < z)
+
+#esercizio nuemro 11
+x = 25
+y = float(x)
+print('il numero è:',y)
+
+#esercizio numero 12
+x = '7500'
+y = int(x)
+print('il numero è:', y)
+
+#esercizio numero 13
+print (bool(35))
+print (bool(0))
+print (bool(-10))
+print (bool(""))
