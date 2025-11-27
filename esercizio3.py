d5ad9ec705b6b238954e6362fad93c3f85c464c4
orione99 <mazzotta.gianni@gmail.com>
2025-11-27 17:54:04 +0100

    Create esercizio3.py

---
+++ esercizio3.py
@@ -0,0 +1,8 @@
+#richiedo un nmero intero
+#converto in decimale e stampo
+richiesta = float(input('inserisci un numero intero'))
+print(richiesta)
+
+#converto il decimale in stringa e stampo
+num_stringa = str(richiesta)
+print('adesso il numero',num_stringa, 'è una stringa')
